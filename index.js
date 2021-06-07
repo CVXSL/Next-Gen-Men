eb55ad84d3fdc42794f7b0415d5161d752007299
jinxy <70546159+JinxGT@users.noreply.github.com>
2021-06-07 17:25:49 -0600

    1.3.2

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ bot.on('ready', () => {
     
     bot.channels.cache.get('851575942795100208').send(`I have restarted!`)
     
-    bot.user.setActivity("Update 1.3.1", {
+    bot.user.setActivity("Update 1.3.2", {
         type: "STREAMING",
         url: "https://www.youtube.com/watch?v=dQw4w9WgXcQ" 
     });
