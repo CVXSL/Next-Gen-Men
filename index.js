22624ecca12961f4d0de1d219d984cdadeb3080f
jinxy <70546159+JinxGT@users.noreply.github.com>
2021-06-07 16:57:28 -0600

    Update index.js

--- index.js
+++ index.js
@@ -32,6 +32,40 @@ bot.on("message", async (message) => {
     }
 });
 
+//love Command
+bot.on('message', async message => {
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
+    const tdc = bot.guilds.cache.get('840284368425582663');
+    if (command === "love") {
+        if (message.author.bot) return;
+        if (!message.member.hasPermission('MANAGE_MESSAGES')) return message.channel.send(`Only staff members can use this command.`);
+        if (message.content.indexOf(prefix) !== 0) return;
+        try {
+            let userID = (args[0] || message.author.id).toString();
+
+            userID = userID.replace(/[^0-9]/g, '');
+
+            const member = tdc.members.cache.get(userID);
+            const content = args.join(' ').replace(`<@!${userID}> `, '')
+
+            if (!member) return message.channel.send('Unable to find that user');
+
+            const embed = new Discord.MessageEmbed()
+            embed.setColor('');
+            embed.setTitle(`You have received an anonymous message!`);
+            embed.setDescription(`You are loved!`);
+//            embed.setFooter('You can DM this bot if you have any questions, comments or concerns. I will send your message to NGM staff.');
+
+            member.send(embed);
+
+            message.channel.send(`Sorry for the two pings <@${userID}>, check your DMs, someone has sent you an anonymous message!`)
+        } catch (e) {
+            message.channel.send(e.toString());
+        }
+    }
+})
+
 // THIS IS THE bot.login
 
 bot.login(process.env.token);
