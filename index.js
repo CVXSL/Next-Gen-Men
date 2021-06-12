6c0ee8436c95d3f05ccecf124b22fe32297f7dcd
CVXSL <70546159+CVXSL@users.noreply.github.com>
2021-06-11 18:49:18 -0600

    Update index.js

--- index.js
+++ index.js
@@ -6,19 +6,6 @@ const prefix = "="
 // THIS IS THE STATUS
 
 bot.on('ready', () => {
-    console.log(`Logged in as ${bot.user.tag} :)`);
-    
-    bot.channels.cache.get('851575942795100208').send(`No errors, I have restarted!`)
-    
-    bot.user.setActivity("Update 1.7.0", {
-        type: "STREAMING",
-        url: "https://www.youtube.com/watch?v=dQw4w9WgXcQ" 
-    });
-
-}
-)
-
-bot.on('ready', () => {
     console.log(`Logged in as ${bot.user.tag} c:`);
 	
     bot.channels.cache.get('851575942795100208').send(`No errors, I have restarted!`)
