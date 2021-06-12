4db4cb3fbfeb726b3bc5f7b8d11227f98afa90a8
CVXSL <70546159+CVXSL@users.noreply.github.com>
2021-06-11 19:01:15 -0600

    1.7.0

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ bot.on('ready', () => {
 	
     bot.channels.cache.get('851575942795100208').send(`No errors, I have restarted!`)
 	
-    bot.user.setActivity("Update 1.6.0", {
+    bot.user.setActivity("Update 1.7.0", {
         type: "STREAMING",
         url: "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
     });
